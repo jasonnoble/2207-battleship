b50e598a29f053a73b2319e9d23281e9d228df07
Jason Noble <jason.noble@itron.com>
2022-07-16 12:09:53 -0600

    Iteration 1: Cell spec for placing ships

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -6,11 +6,37 @@ RSpec.describe Cell do
   let(:cruiser) { Ship.new("Cruiser", 3) }
 
   describe '#new(coordinate)' do
-    it 'creates a new cell' 
+    it 'creates a new cell' do
+      expect(cell).to be_a(Cell)
+    end
   end
-  describe '#coordinate'
-  describe '#ship'
-  describe '#empty?'
-  describe '#place_ship(ship)'
+  describe '#coordinate' do
+    it 'returns the cell coordinate' do
+      expect(cell.coordinate).to eq("B4")
+    end
+  end
+  describe '#ship' do
+    it 'returns the ship' do
+      expect(cell.ship).to eq(nil)
+    end
+  end
+  describe '#empty?' do
+    context 'when a ship has not been placed' do
+      it 'returns false' do
+        expect(cell).to be_empty
+      end
+    end
+    context 'when a ship has been placed' do
+      it 'returns true' do
+        cell.place_ship(cruiser)
+        expect(cell).not_to be_empty
+      end
+    end
+  end
+  describe '#place_ship(ship)' do
+    it 'places the ship' do
+      expect{ cell.place_ship(cruiser) }.to change(cell, :empty?).from(true).to(false)
+    end
+  end
 
 end
